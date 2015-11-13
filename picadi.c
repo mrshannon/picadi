c1b449190f9fdf8d3c2667278b936cfa10caf942
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2015-11-12 22:16:41 -0700

    Add template main file.

---
+++ picadi.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   picadi.c
+ * Author: mrshannon
+ *
+ * Created on November 12, 2015, 10:11 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ src/picadi.c
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////
+// PIC ADI (Attitude Direction Indicator)
+////////////////////////////////////////////////////////////////////////
+// File: pacadi.c
+// Author: Michael R. Shannon
+// Written: Thursday, November 12, 2015
+// Updated: Thursday, November 12, 2015
+// Device: PIC18F87K22
+// Fosc: 10 MHz
+// Instruction Clock: Fosc/4
+//
+////////////////////////////////////////////////////////////////////////
+
+
+#include <p18cxxx.h>
+#pragma config FOSC=HS1, PWRTEN=ON, BOREN=ON, BORV=2, PLLCFG=OFF
+#pragma config WDTEN=OFF, CCP2MX=PORTC, XINST=OFF
+
+
+// Declaration of ISRs.
+void isrh(void);
+void isrl(void);
+
+
+
+// High priority interrupt redirect.
+#pragma code highVector=0x08
+void highVector(void){
+    _asm GOTO isrh _endasm;
+}
+#pragma code
+
+
+// Low priority interrupt redirect.
+#pragma code lowVector=0x18
+void lowVector(void){
+    _asm GOTO isrl _endasm;
+}
+#pragma code
+
+
+void main(){
+
+    ////////////////////////////////////////////////////////////////////
+    //// INITIALIZATION
+    ////////////////////////////////////////////////////////////////////
+
+
+    ////////////////////////////////////////////////////////////////////
+    //// MAIN LOOP
+    ////////////////////////////////////////////////////////////////////
+    while (1){
+
+    }
+}
+
+
+// Description
+//      The high priority interrupt handler.
+//
+#pragma interrupt isrh nosave=TBLPTR,TABLAT,PCLATH,PCLATU,\
+                              section(".tmpdata"),section("MATH_DATA")
+void isrh(void){
+    // high priority interrupt code
+}
+
+
+// Description:
+//      The low priority interrupt handler.
+//
+#pragma interruptlow isrl nosave=TBLPTR,TABLAT,PCLATH,PCLATU,\
+                                 section(".tmpdata"),section("MATH_DATA")
+void isrl(void){
+    // low priority interrupt code
+}
