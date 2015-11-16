2364e5e32e8a63a02e5ac4538c43211ad24eae48
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2015-11-15 21:47:26 -0700

    Add configuration header file.

---
+++ include/config.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////
+// File: config.h
+// Author: Michael R. Shannon
+// Written: Sunday, November 15, 2015
+// Updated: Sunday, November 15, 2015
+// Description:
+//      Header file for general configuration.
+//
+////////////////////////////////////////////////////////////////////////
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// PLL multiplier for use with delay functions.  Set to 1 when not using
+// PLL and 4 when using PLL.
+#define PLLMUL 1
+// #define PLLMUL 4
+
+#endif // CONFIG_H
+
