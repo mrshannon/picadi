0f7971680f6682fbdcee1c09c812a9f22471b347
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2015-11-30 12:43:12 -0700

    Remove abs functions from util.
    The change was made in the source files earlier but the abs functions were
    accidentally left in the header files.

--- include/util.h
+++ include/util.h
@@ -2,7 +2,7 @@
 // File: util.h
 // Author: Michael R. Shannon
 // Written: Sunday, November 15, 2015
-// Updated: Sunday, November 29, 2015
+// Updated: Monday, November 30, 2015
 // Device: PIC18F87K22
 // Compiler: C18
 // Fosc: 10 MHz
@@ -52,20 +52,29 @@ void delayxs(uint8_t s);
 
 
 // Description:
-//      Functions to compute the absolute value of each signed integer
-//      type.
+//      Wrapper to fix type of literal strings.
 //
 // Input:
-//      intX_t n:
-//          Number to take absolute value of.
-// 
-// Output (intX_t):
-//      Absolute value of n.
+//      str:
+//          A string literal.
 //
-int8_t abs8(int8_t n);
-int16_t abs16(int16_t n);
-int24_t abs24(int24_t n);
-int32_t abs32(int32_t n);
+#define STR(str) ((const rom far char *)str)
+
+
+// Description:
+//      Function to load a literal string into a global RAM buffer for
+//      use in string functions.
+//
+// Input:
+//      const rom char *src:
+//          A Sting located in program memory.
+//
+// Output (char *):
+//      A pointer to a global buffer where the source string has been
+//      copied.
+//
+#define STR_MAX_LENGTH 256
+char *str(const rom char *src);
 
 
 #endif // UTIL_H
